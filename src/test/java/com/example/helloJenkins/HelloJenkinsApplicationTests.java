135b6c22de7a52b1d39f68cc8de7e178d105fbac
ssoyeong <jsy171771@gmail.com>
2022-10-20 13:49:31 +0900

    Initial commit

---
+++ src/test/java/com/example/helloJenkins/HelloJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
